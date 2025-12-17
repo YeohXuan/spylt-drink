0c7f258b5ac86d4faba59820855912de4e6a12a3
Yeoh Xuan <yorkxuan7@gmail.com>
2025-12-17 22:11:49 +0800

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "../sections/HeroSection";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+      <img
+        src="public/images/nav-logo.svg"
+        alt="nav-logo"
+        className="md:w-24 w-20"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
